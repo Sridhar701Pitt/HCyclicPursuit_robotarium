af6cf5b79d4f05373b2267ee58bd5b1ae65ea598
Sridhar701Pitt <sridhar.pitt@gmail.com>
2021-11-23 20:49:30 -0500

    Hierarchial Cyclic Pursuit v0.2 - Added leader robot - Increased size of background to fill the projector screen - Updates initial positions to match the actual initial positions - Max Speed 4/4 - Bigger L1 and smaller L2

--- CyclicPursuit_example.m
+++ CyclicPursuit_example.m
@@ -1,37 +1,46 @@
 close all, clc
 %clear all
 N=3;                        % Number of agents
+Lead = 1;                      % Number of leaders 
 dt=0.01;                   % numerical steplength
 max_iter = 10000;                           
 
 % Initialize robotarium
-initial_positions = generate_initial_conditions(N, 'Width', 1, 'Height', 1,'Spacing', 0.1);
-rbtm = Robotarium('NumberOfRobots', N, 'ShowFigure', true, 'InitialConditions', initial_positions);
+initial_positions = generate_initial_conditions(N+Lead, 'Width', 1, 'Height', 1,'Spacing', 0.1);
+rbtm = Robotarium('NumberOfRobots', N+Lead, 'ShowFigure', true, 'InitialConditions', initial_positions);
 % rb = RobotariumBuilder();
 % rbtm = rb.set_number_of_agents(N).set_save_data(false).build();
 si_to_uni_dyn = create_si_to_uni_dynamics();
 % Single-integrator barrier certificates
-uni_barrier_cert = create_uni_barrier_certificate_with_boundary();
+uni_barrier_cert = create_uni_barrier_certificate_with_boundary('SafetyRadius', 0.06,'BarrierGain', 300);
 
 % Initialize robots
 xuni = rbtm.get_poses();                                    % States of real unicycle robots
-x = xuni(1:2,:);                                            % x-y positions only
-rbtm.set_velocities(1:N, zeros(2,N));                       % Assign dummy zero velocity
+x = xuni(1:2,1:N+Lead);                                            % x-y positions only
+rbtm.set_velocities(1:N+Lead, zeros(2,N+Lead));                       % Assign dummy zero velocity
 rbtm.step();                                                % Run robotarium step
 
 % Cyclic graph
 A = diag(ones(N-1,1),-1);
 A(1,N) = 1; 
-L = diag(sum(A)) - A;
+followers = - diag(sum(A)) + A;
+L = zeros(N+Lead, N+Lead);
+L(2:N+Lead, 2:N+Lead) = followers;
+L(2, 2) = L(2, 2) + 1;
+L(3, 3) = L(3, 3) + 1;
+L(4, 4) = L(4, 4) + 1;
+L(2:N+Lead, 1) = -1;
 
 % Target cycle definition
 center = [0;0];
-radiusL1 = 0.4; % Level 1 radius
-radiusL2 = 0.4; % Level 2 Radius
-interAgentDistance = radiusL2*2*sin(pi/N);
+radiusL1 = 0.6; % Level 1 radius
+radiusL2 = 0.35; % Level 2 Radius
+interAgentDistance = radiusL2*2*sin(pi/(N+Lead - 1));
 kp1 = 7; % Gain to maintain circular formation and distance
-kp2 = 0.7; % Gain to stay close to the centroid
-kp3 = 0.7; % velocity Gain to follow to neighbour (based on distance) 
+kp2 = 1.5; % Gain to follow the centroid
+kp3 = 0.7; % velocity Gain to follow to neighbour (based on distance)
+kpL = 1;
+robot_maxSpeedFactor = 3/4;
 
 %Display iteration and time and title
 start_time = tic; %The start time to compute time elapsed.
@@ -39,14 +48,14 @@ font_size = determine_font_size(rbtm, 0.04);
 iteration_caption = sprintf('Iteration %d', 0);
 time_caption = sprintf('Time Elapsed %0.2fs', toc(start_time));
 loop_caption = sprintf('Loop Time %0.2fms', 0);
-title_caption = sprintf('Hierarchical Cyclic Pursuit \nTest 0.1');
+title_caption = sprintf('Hierarchical Cyclic Pursuit \nTest 0.2');
 iteration_label = text(-1.5, -0.7, iteration_caption, 'FontSize', font_size, 'Color', 'w','FontName','FixedWidth');
 time_label = text(-1.5, -0.9, time_caption, 'FontSize', font_size, 'Color', 'w','FontName','FixedWidth');
 loop_label = text(-1.5, -0.8, loop_caption, 'FontSize', font_size, 'Color', 'w','FontName','FixedWidth');
 title_label = text(-1.5, 0.85, title_caption, 'FontSize', font_size, 'Color', 'w','FontName','FixedWidth','FontWeight','bold');
 
 %DisplayParams
-param_label = text(-1.5, -0.0, sprintf('Parameters\nRadius L1 %0.2f\nRadius L2 %0.2f\nkp1 %0.2f\nkp2 %0.2f\nkp3 %0.2f', radiusL1, radiusL2,kp1,kp2,kp3), 'FontSize', font_size, 'Color', 'w','FontName','FixedWidth');
+param_label = text(-1.5, -0.0, sprintf('Parameters\nRadius L1 %0.2f\nRadius L2 %0.2f\nkp1 %0.2f\nkp2 %0.2f\nkp3 %0.2f\nkpL %0.2f\nSpeed factor %0.2f', radiusL1, radiusL2,kp1,kp2,kp3,kpL,robot_maxSpeedFactor), 'FontSize', font_size, 'Color', 'w','FontName','FixedWidth');
 
 centerData = plot(center(1),center(2),'*w','markersize',10);
 centeroidData = plot(center(1),center(2),'*y','markersize',10);
@@ -58,16 +67,17 @@ plot(radiusL1.*cos(th)+center(1),radiusL1.*sin(th)+center(2),'b')
 % Import and scale the nightsky appropriately.
 gt_img = imread('nightsky.png'); % Original input image file
 % Display the image with an associated spatial referencing object.
-x_img = linspace(-1.6, 1.6, size(gt_img,2));
-y_img = linspace(1.0, -1.0, size(gt_img,1)); %Note the 1 to -1 here due to the (1,1) pixel being in the top left corner.
+x_img = linspace(-2.0, 2.0, size(gt_img,2));
+y_img = linspace(1.5, -1.5, size(gt_img,1)); %Note the 1 to -1 here due to the (1,1) pixel being in the top left corner.
 gt_img_handle = image(x_img, y_img, gt_img,'CDataMapping','scaled');
 
 % Import and scale the earth logo appropriately.
-wlte_img = imresize(imread('wlte.png'),0.2); % Original input image file
+wlte_img = imresize(imread('wlte.png'),0.3); % Original input image file
 
 % Display the image with an associated spatial referencing object.
-x_img = linspace(-0.1, 0.1, size(wlte_img,2));
-y_img = linspace(0.11, -0.11, size(wlte_img,1)); %Note the 1 to -1 here due to the (1,1) pixel being in the top left corner.
+img_scaleFactor = 1.6;
+x_img = linspace(-0.1*img_scaleFactor, 0.1*img_scaleFactor, size(wlte_img,2));
+y_img = linspace(0.11*img_scaleFactor, -0.11*img_scaleFactor, size(wlte_img,1)); %Note the 1 to -1 here due to the (1,1) pixel being in the top left corner.
 wlte_img_handle = image(x_img, y_img, wlte_img,'CDataMapping','scaled');
 
 uistack(wlte_img_handle, 'bottom')
@@ -77,13 +87,15 @@ uistack([time_label], 'top'); % Time label is above iteration label.
 
 % Reach initial positions on a circle
 if 1        
-    circularTargets = [ radiusL2*cos( 0:2*pi/N:2*pi*(1- 1/N) ) ; radiusL2*sin( 0:2*pi/N:2*pi*(1- 1/N) ) ];
+    circularTargets = [ radiusL2*cos( 0:2*pi/N:2*pi*(1- 1/N) ) + radiusL1 ; radiusL2*sin( 0:2*pi/N:2*pi*(1- 1/N) ) + 0 ];
+    leader_Target = [radiusL1; 0];
+    circularTargets = [leader_Target circularTargets];
     errorToInitialPos = x - circularTargets;                % Error
     errorNorm = [1,1]*(errorToInitialPos.^2);               % Norm of error
-    while max( errorNorm ) > 0.05
+    while max( errorNorm ) > 0.01
         % Update state variables        
         xuni = rbtm.get_poses();                            % States of real unicycle robots
-        x = xuni(1:2,:);                                    % x-y positions
+        x = xuni(1:2,1:N+Lead);                                    % x-y positions
         
         % Update errors
         errorToInitialPos = x - circularTargets;
@@ -92,15 +104,15 @@ if 1
         % Conput control inputs
         dxi = -0.3.*errorToInitialPos;
         % To avoid errors, we need to threshold dxi
-        norms = arrayfun(@(x) norm(dxi(:, x)), 1:N);
-        threshold = 4/4*rbtm.max_linear_velocity;
+        norms = arrayfun(@(x) norm(dxi(:, x)), 1:N+Lead);
+        threshold = 2/4*rbtm.max_linear_velocity;
         to_thresh = norms > threshold;
         dxi(:, to_thresh) = threshold*dxi(:, to_thresh)./norms(to_thresh);
 
         dxu = si_to_uni_dyn(dxi, xuni);
         dxu = uni_barrier_cert(dxu, xuni);
         % Assing new control inputs to robots
-        rbtm.set_velocities(1:N, dxu);                       % Assign dummy zero velocity
+        rbtm.set_velocities(1:N+Lead, dxu);                       % Assign dummy zero velocity
         
         rbtm.step();                                        % Run robotarium step
     end
@@ -143,11 +155,11 @@ for k = 1:max_iter
     xuni = rbtm.get_poses();                                % Get new robots' states
     x = xuni(1:2,:);                                        % Extract single integrator states
     %Find current centroid
-    Xc = 1/N*x*ones(N,1);
+    Xc = 1/(N+Lead - 1)*x(:,2:N+Lead)*ones(N+Lead - 1,1);
     set(centeroidData,'XData',Xc(1),'YData',Xc(2))
     %Rotate the earth
     if mod(k,2) == 0
-        set(wlte_img_handle,'CData', imrotate(wlte_img,k/20,'crop'));
+        set(wlte_img_handle,'CData', imrotate(wlte_img,k/5,'crop'));
     end
     % Update Iteration and Time marker
     iteration_caption = sprintf('Iteration %d', k);
@@ -169,28 +181,43 @@ for k = 1:max_iter
     % does not change size).
     
     %Actual calculation
-    dx = zeros(2,N);                                           % Initialize velocities to zero         
-    for i = 1:N                
-        for j = find(A(:,i))'
+    dx = zeros(2,N+Lead);                                           % Initialize velocities to zero         
+    %Leader follows the center
+    
+    dx(:,1) = dx(:,1) + kpL*(center - x(:,1));
+    
+    for i = 2:N+Lead % loop for followers
+        neighbors = topological_neighbors(L, i);
+        for j = neighbors
             if ~isempty(j)
-                alpha =  pi/N + kp1*(interAgentDistance - norm(x(:,j)-x(:,i)) );
-                % limit alpha to -pi/2 to pi/2
-                alpha = min(max(alpha,-pi/2),pi/2);
-                R = [cos(alpha), sin(alpha); -sin(alpha) cos(alpha)];
-                dx(:,i) = dx(:,i) + kp3*R*( x(:,j)-x(:,i) ) + kp2*(center - x(:,i));
+%                 alpha =  pi/N + kp1*(interAgentDistance - norm(x(:,j)-x(:,i)) );
+%                 % limit alpha to -pi/2 to pi/2
+%                 alpha = min(max(alpha,-pi/2),pi/2);
+%                 R = [cos(alpha), sin(alpha); -sin(alpha) cos(alpha)];
+%                 dx(:,i) = dx(:,i) + kp3*R*( x(:,j)-x(:,i) ) + kp2*(x(:,1) - x(:,i));
+                if j == 1
+                    distanceToLeaderE = norm(x(:, 1) - x(:,i));
+                    dx(:,i) = dx(:, i) + kp2*((distanceToLeaderE - radiusL2)/distanceToLeaderE).*(x(:,1) - x(:,i));
+                else
+                    alpha =  pi/N + kp1*(interAgentDistance - norm(x(:,j)-x(:,i)) );
+                    % limit alpha to -pi/2 to pi/2
+                    alpha = min(max(alpha,-pi/2),pi/2);
+                    R = [cos(alpha), sin(alpha); -sin(alpha) cos(alpha)];
+                    dx(:,i) = dx(:,i) + kp3*R*( x(:,j)-x(:,i) );
+                end
             end
         end
     end
     dxi = dx;
     % To avoid errors, we need to threshold dxi
-    norms = arrayfun(@(x) norm(dxi(:, x)), 1:N);
-    threshold = 3/4*rbtm.max_linear_velocity;
+    norms = arrayfun(@(x) norm(dxi(:, x)), 1:N+Lead);
+    threshold = robot_maxSpeedFactor*rbtm.max_linear_velocity;
     to_thresh = norms > threshold;
     dxi(:, to_thresh) = threshold*dxi(:, to_thresh)./norms(to_thresh);
 
     dxu = si_to_uni_dyn(dxi, xuni);                            % Convert single integrator inputs into unicycle inputs
     dxu = uni_barrier_cert(dxu, xuni);
-    rbtm.set_velocities(1:N, dxu); rbtm.step();              % Set new velocities to robots and update
+    rbtm.set_velocities(1:N+Lead, dxu); rbtm.step();              % Set new velocities to robots and update
     loop_time = toc(loop_start_time)*1000;
     
 end
